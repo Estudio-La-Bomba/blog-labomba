1d76c8c3bf7e0866057f1aea0531da2c9b78c9d9
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-11-17 16:24:29 -0300

    chore: add date in list blog posts

--- index.php
+++ index.php
@@ -6,30 +6,32 @@
 
 <main class="articles">
 
-<?php $query = new WP_Query(array('post_type' => 'post', 'posts_per_page' => -1));?>
- <?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post();?>
+  <?php $query = new WP_Query(array('post_type' => 'post', 'posts_per_page' => -1));?>
+  <?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post();?>
 
-		          <div class="articles-single">
-		            <a href="<?php the_permalink();?>"><?php the_post_thumbnail()?></a>
+  <div class="articles-single">
+    <a href="<?php the_permalink();?>"><?php the_post_thumbnail()?></a>
 
-		            <a href="<?php the_permalink();?>">
-		              <h2 class="aticles-single-title">
-		                <?php the_title();?>
-		              </h2>
-		            </a>
+    <a href="<?php the_permalink();?>">
+      <h2 class="aticles-single-title">
+        <?php the_title();?>
+      </h2>
+    </a>
 
-		            <small class="articles-single-tags"><?php the_tags('', ', ')?></small>
+    <small class="articles-single-tags"><?php the_tags('', ', ')?></small>
 
-		            <p><?php the_excerpt();?></p>
-		          </div>
+    <p><?php the_excerpt();?></p>
 
-		          <?php endwhile;
+    <small class="article-date"><?php the_date()?></small>
+  </div>
+
+  <?php endwhile;
     wp_reset_postdata();
 else: ?>
- <p><?php esc_html_e('Desculpe, não temos nenhum post nessa categoria.');?></p>
- <?php endif;?>
+  <p><?php esc_html_e('Desculpe, não temos nenhum post nessa categoria.');?></p>
+  <?php endif;?>
 
-  </main>
+</main>
 </div>
 
 <?php get_footer();?>
